9bd14cfafd196d70496c5dbbae09654a695e8f95
Tarık Sümer <tariksumer47@outlook.com>
2024-08-20 12:08:32 +0300

    first commit, no update

---
+++ src/main/java/com/example/TarikSumer/SbWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.TarikSumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TarikSumer/SbWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TarikSumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
